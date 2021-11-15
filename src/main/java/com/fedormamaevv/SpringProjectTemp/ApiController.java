2bdff92773540e77952ffe9f483cf798400446c4
FedMam <fedor_mamaev_v@mail.ru>
2021-11-15 18:11:47 +0300

    Initial commit

---
+++ src/main/java/com/fedormamaevv/SpringProjectTemp/ApiController.java
@@ -0,0 +1,9 @@
+package com.fedormamaevv.SpringProjectTemp;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+public class ApiController {
+
+}
---
+++ src/main/java/com/fedormamaevv/SpringProjectTemp/SpringProjectTempApplication.java
@@ -0,0 +1,13 @@
+package com.fedormamaevv.SpringProjectTemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectTempApplication.class, args);
+	}
+
+}
